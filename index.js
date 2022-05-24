7c558eb841509a19b6c60dfdc85ae32734af4bd5
Shamim <yeasinshamim7@gmail.com>
2022-05-24 10:57:26 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w8qsv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const collection = client.db("car-parts").collection("parts");
+        console.log('db connect');
+    }
+    finally { }
+}
+run().catch(console.dir);
+
 
 app.get('/', async (req, res) => {
     console.log('car parts manufacture server running.');
